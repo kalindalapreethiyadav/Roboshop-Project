5f41311c37c3e6a3993621e2d284b412a87d5ded
preethi k <kpreethiyadav579@gmail.com>
2022-07-17 22:29:05 +0530

    adding update

--- components/cart.sh
+++ components/cart.sh
@@ -1,5 +1,17 @@
 #!/bin/bash/
-
+#if any line of cmd code have error then exit the script
+set -e 
+COMPONENT=cart
+#All output need to be redirected to log file 
+LOGFILE="/tmp/$COMPONENT.log"
+APPNAME=nodejs
+PROJECTUSER=roboshop
+APP_REPOS_URL="https://rpm.nodesource.com/setup_lts.x"
 #lets call all common function fucntions for validating and other common func for all componets
 source components/common.sh
-echo "cart I am inside"  
+
+echo "***************************************"
+
+Nodejs
+
+echo -e "\e[36m ******Succesfully completed Configuration*************\e[0m"
