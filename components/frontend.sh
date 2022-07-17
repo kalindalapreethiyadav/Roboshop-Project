daa53b895a48fd7677c0eb38c5a4d4a2566d65b4
preethi k <kpreethiyadav579@gmail.com>
2022-07-17 13:10:52 +0530

    adding updata

--- components/frontend.sh
+++ components/frontend.sh
@@ -1,18 +1,18 @@
 #!/bin/bash/
 #if any line of cmd code have error then exit the script
 set -e 
-
+COMPONENT=frontend
+#All output need to be redirected to log file 
+LOGFILE="/tmp/$COMPONENT.log"
 #lets call all common function fucntions for validating and other common func for all componets
 source components/common.sh
 
-#All output need to be redirected to log file 
-COMPONENT=frontend
-LOGFILE="/tmp/$COMPONENT.log"
-
+echo  "**************************************"
 echo -n "Installing NGINX"
 yum install nginx -y &>> $LOGFILE
 stat
 
+echo "***************************************"
 
 systemctl enable nginx
 systemctl start nginx
