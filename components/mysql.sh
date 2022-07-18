8c30b95f6ab83539658b293a8453187265a6e124
preethi k <kpreethiyadav579@gmail.com>
2022-07-18 21:04:04 +0530

    adding update

--- components/mysql.sh
+++ components/mysql.sh
@@ -27,11 +27,11 @@ stat
 #SQL root password default
 #if above statment able to conenct using new password then not required to perform below steps
 echo -n "stating $COMPONENT validation : "
+#echo "show databases" | echo mysql -uroot -pRoboShop@1 &>> $LOGFILE
 #if [ 0 -ne $? ] ; then
     echo -n "Configuring SQL default password :"
     # we are saving the Query of new root password change and saving in a file.sql
     echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" > /tmp/root_password_change.sql
-    stat
     # 1.first we are finding out the default temperory password on MYSQL in mysqlid
     Default_root_password=$(sudo grep "root@localhost" /var/log/mysqld.log | awk -F: '{print $NF}' | awk -F ';' '{print $1}')
     stat
