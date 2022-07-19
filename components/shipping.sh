5b45e9c8ac2fee2d4bfc6ae79e0e91ff3c982bb6
preethi k <kpreethiyadav579@gmail.com>
2022-07-19 23:02:36 +0530

    adding updata

--- components/shipping.sh
+++ components/shipping.sh
@@ -1,25 +1,17 @@
+#!/bin/bash/
+#if any line of cmd code have error then exit the script
+set -e 
+COMPONENT=maven
+#All output need to be redirected to log file 
+LOGFILE="/tmp/$COMPONENT.log"
+APPNAME=maven
+PROJECTUSER=roboshop
+APP_REPOS_URL="https://rpm.nodesource.com/setup_lts.x"
+#lets call all common function fucntions for validating and other common func for all componets
+source components/common.sh
 
+echo "***************************************"
 
-# yum install maven -y
+Installing_Maven
 
-# useradd roboshop
-
-
-$ cd /home/roboshop
-$ curl -s -L -o /tmp/shipping.zip "https://github.com/stans-robot-project/shipping/archive/main.zip"
-$ unzip /tmp/shipping.zip
-$ mv shipping-main shipping
-$ cd shipping
-$ mvn clean package 
-$ mv target/shipping-1.0.jar shipping.jar
-
-$ vim systemd.service 
-
-# mv /home/roboshop/shipping/systemd.service /etc/systemd/system/shipping.service
-# systemctl daemon-reload
-# systemctl start shipping 
-# systemctl enable shipping
-# systemctl status shipping -l 
-( You should see a message stating, that shipping started )
-
-Ref: Started ShippingServiceApplication in `X` seconds
+echo -e "\e[36m ******Succesfully completed Configuration*************\e[0m"
