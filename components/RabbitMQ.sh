d1dc42024b2679c3a82eb3cc8e4f7df528eef379
preethi k <kpreethiyadav579@gmail.com>
2022-07-22 14:42:36 +0530

    adding updata

--- components/RabbitMQ.sh
+++ components/RabbitMQ.sh
@@ -11,7 +11,7 @@ REPOS_URL="https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erla
 source components/common.sh
 
 echo -n "installing $APPNAME dependency: "
-yum install $REPOS_URL -y &>> $LOGFILE
+yum install "https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm" -y &>> $LOGFILE
 stat
 
 echo -n "configuring $APPNAME Repository: "
