3a0a2fa53beaab54897c5476baa6b57c12fb406f
preethi k <kpreethiyadav579@gmail.com>
2022-07-22 17:36:41 +0530

    adding updata

--- components/Dispatch.sh
+++ components/Dispatch.sh
@@ -10,28 +10,8 @@ REPOS_Link="https://github.com/stans-robot-project/frontend/archive/main.zip"
 #lets call all common function fucntions for validating and other common func for all componets
 source components/common.sh
 
-echo "***************************************"
-echo -n "Installing $APPNAME"
-yum install $APPNAME -y &>> $LOGFILE
-stat
-
-#calling cretae user function 
-Create_User
-
-#calling Download and extract function
-Download_and_Extract
-stat
-
-echo -n "checking golang commands :"
-$ go mod init $COMPONENT
-$ go get 
-$ go build
-stat
-
-echo -n "Enable and start $COMPONENT $APPNAME Service"
-systemctl enable $APPNAME &>> $LOGFILE
-systemctl start $APPNAME &>> $LOGFILE
-stat
+#calling golang function
+go_lang
 
 
 echo -e "\e[36m ******Succesfully completed Configuration*************\e[0m"
