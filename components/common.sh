57c7542d867280883af7da90ab2013676898b250
preethi k <kpreethiyadav579@gmail.com>
2022-07-22 16:31:51 +0530

    adding updata

--- components/common.sh
+++ components/common.sh
@@ -146,8 +146,8 @@ stat
 echo -e  "configuration update for the user and group id:"
 User_id=$(id -u)
 Group_id=$(id -g)
-sed -i -e "/uid/ c uid = $User_id" $COMPONENT.ini &>> $LOGFILE
-sed -i -e "/gid/ c gid = $Group_id" $COMPONENT.ini &>> $LOGFILE
+sed -i -e "/uid/ c uid = $User_id" payment.ini
+sed -i -e "/gid/ c gid = $Group_id" payment.ini
 stat
 
 #Update SystemD service file with CART , USER , RABBITMQ Server IP Address.
