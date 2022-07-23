47773f9dae085a65ee08427ac4499ed0d5c3b101
preethi k <kpreethiyadav579@gmail.com>
2022-07-23 23:06:54 +0530

    adding updata

--- components/create-server.sh
+++ components/create-server.sh
@@ -9,4 +9,4 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=CloudDevOps-LabImag
 #echo $AMI_ID
 
 # creating the instances unsing AWS CLI commands
- aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"  | jq
+ aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]'  | jq
