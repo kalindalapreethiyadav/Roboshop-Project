6eed57a4e1de557b8cf0ecf46db215b9bd20658e
preethi k <kpreethiyadav579@gmail.com>
2022-07-24 00:08:24 +0530

    adding updata

--- components/create-server.sh
+++ components/create-server.sh
@@ -6,18 +6,24 @@ COMPONENT=$1
 SGID="sg-09f0434c8144d66e5"
 #Script Aim is to create a vm instance and records creation and update to host zone 
 #lets find out the AMI id of the AMI 
+
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=CloudDevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')
-#echo $AMI_ID
+echo $AMI_ID
 
-# creating the instances unsing AWS CLI commands
-PRIVATE_IP=$(aws ec2 run-instances --security-group-ids $SGID --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+create_server() {
+    echo "$COMPONENT Server Creation in progress"
+    PRIVATE_IP=$(aws ec2 run-instances --security-group-ids $SGID --image-id  $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
+    # # Changing the IP Address and DNS Name as per the component
+    sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json > /tmp/record.json 
+    aws route53 change-resource-record-sets --hosted-zone-id Z09626353E72G6GNQ0R5A --change-batch file:///tmp/record.json | jq 
+}
 
-#Create a ChangeResourceRecordSets request
-#CREATE creates a record set with a specified value in the hosted zone
-#DELETE deletes a record set with a specified value in the hosted zone
-#UPSERT either creates a new record set with a specified value, or updates a record set with a specified value if that record set already exists
-
-sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json > /tmp/record.json 
-
-aws route53 change-resource-record-sets --hosted-zone-id Z09626353E72G6GNQ0R5A --change-batch file:///tmp/record.json | jq 
+if [ "$1" == "all" ]; then 
+    for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis ; do 
+        COMPONENT=$COMPONENT
+        create_server 
+    done 
+else 
+    create_server # Calling a function 
+fi
