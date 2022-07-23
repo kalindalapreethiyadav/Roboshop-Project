f473b74c8c6e1ddb676fcd694dd2d2e274b1929f
preethi k <kpreethiyadav579@gmail.com>
2022-07-23 23:26:36 +0530

    adding updata

--- components/create-server.sh
+++ components/create-server.sh
@@ -3,11 +3,11 @@
 
 #This script to create a server and assign records to the route53 host zone
 COMPONENT=$1
-SGroup_ID=
+SGroup_ID="sg-09f0434c8144d66e5"
 #Script Aim is to create a vm instance and records creation and update to host zone 
 #lets find out the AMI id of the AMI 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=CloudDevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')
 #echo $AMI_ID
 
 # creating the instances unsing AWS CLI commands
- aws ec2 run-instances --security-group-ids $SGID --image-id  $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
+ aws ec2 run-instances --security-group-ids $SGID --image-id  $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
