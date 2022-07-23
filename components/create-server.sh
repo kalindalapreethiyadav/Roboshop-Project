6f57e3160774174ce03d533c50d9d23d5ac352b7
preethi k <kpreethiyadav579@gmail.com>
2022-07-23 22:44:54 +0530

    adding updata

--- components/create-server.sh
+++ components/create-server.sh
@@ -5,5 +5,5 @@
 
 #Script Aim is to create a vm instance and records creation and update to host zone 
 #lets find out the AMI id of the AMI 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=CloudDevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g)
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=CloudDevOps-LabImage-CentOS7" | jq ".Images[].ImageId" | sed -e 's/"//g')
 echo $AMI_ID
