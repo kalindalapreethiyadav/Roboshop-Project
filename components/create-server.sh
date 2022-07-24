7f663934c9daf27f8627f7081c2405e9aee05929
preethi k <kpreethiyadav579@gmail.com>
2022-07-24 13:23:14 +0530

    adding updata

--- components/create-server.sh
+++ components/create-server.sh
@@ -12,14 +12,14 @@ echo $AMI_ID
 
 create_server() {
     echo "$COMPONENT Server Creation in progress"
-    PRIVATE_IP=$(aws ec2 run-instances --security-group-ids $SGID --image-id $AMI_ID --instance-type t2.micro | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+    PRIVATE_IP=$(aws ec2 run-instances --security-group-ids $SGID --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
     # # Changing the IP Address and DNS Name as per the component
-    sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json > /tmp/route53.json 
-    aws route53 change-resource-record-sets --hosted-zone-id Z037286228DFYMBZCZ58K --change-batch file:///tmp/route53.json | jq 
+    sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json > /tmp/route.json 
+    aws route53 change-resource-record-sets --hosted-zone-id Z037286228DFYMBZCZ58K --change-batch file:///tmp/route.json | jq 
 }
 
-if [ "$1" == "all" ]; then 
+if [ "$1" == "all" ] ; then 
     for component in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis ; do 
         COMPONENT=$COMPONENT
         create_server 
