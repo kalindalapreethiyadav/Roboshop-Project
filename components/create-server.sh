0893b63cdd4d888117955f921056bdf9e94f7cd9
preethi k <kpreethiyadav579@gmail.com>
2022-07-24 13:57:02 +0530

    adding updata

--- components/create-server.sh
+++ components/create-server.sh
@@ -10,13 +10,15 @@ SGID="sg-09f0434c8144d66e5"
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=CloudDevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMI_ID 
 
-create_server() {
-echo -e "${COMPONENT} Server Creation in progress"
-PRIVATE_IP=$(aws ec2 run-instances --security-group-ids $(SGID) --image-id $(AMI_ID) --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$(COMPONENT)}]"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+create_server() 
+{
+echo -e "$COMPONENT Server Creation in progress"
+PRIVATE_IP=$(aws ec2 run-instances --security-group-ids $SGID --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$(COMPONENT)}]"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
 # Changing the IP Address and DNS Name as per the component
-sed -e "s/IPADDRESS/$(PRIVATE_IP)/" -e "s/COMPONENT/$(COMPONENT)/" route53.json > record.json
+sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json > record.json
 aws route53 change-resource-record-sets --hosted-zone-id Z037286228DFYMBZCZ58K --change-batch file://record.json | jq 
+
 }
 
 if [ "$1" == "all" ] ; then 
