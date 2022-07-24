b164163c0ec23aca0f13f89cfdabef601a6d8282
preethi k <kpreethiyadav579@gmail.com>
2022-07-24 13:38:39 +0530

    adding updata

--- components/create-server.sh
+++ components/create-server.sh
@@ -15,8 +15,8 @@ create_server() {
     PRIVATE_IP=$(aws ec2 run-instances --security-group-ids $SGID --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
     # # Changing the IP Address and DNS Name as per the component
-    sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json > record.json
-    aws route53 change-resource-record-sets --hosted-zone-id Z037286228DFYMBZCZ58K --change-batch file://record.json | jq 
+    sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json > /tmp/record.json
+    aws route53 change-resource-record-sets --hosted-zone-id Z037286228DFYMBZCZ58K --change-batch file:////tmp/record.json | jq 
 }
 
 if [ "$1" == "all" ] ; then 
