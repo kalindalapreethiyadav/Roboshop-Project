b3b41f987539618d71db049be3bc522cb886851c
preethi k <kpreethiyadav579@gmail.com>
2022-08-07 18:20:00 +0530

    adding update

--- components/create-server.sh
+++ components/create-server.sh
@@ -4,6 +4,7 @@
 #!/bin/bash/
 #This script to create a server and assign records to the route53 host zone
 COMPONENT=$1
+ENV=$2
 SGID="sg-09f0434c8144d66e5"
 #Script Aim is to create a vm instance and records creation and update to host zone 
 #lets find out the AMI id of the AMI 
@@ -14,10 +15,10 @@ echo $AMI_ID
 create_server() 
 {
 echo -e "$COMPONENT Server Creation in progress"
-PRIVATE_IP=$(aws ec2 run-instances --security-group-ids $SGID --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+PRIVATE_IP=$(aws ec2 run-instances --security-group-ids $SGID --image-id $AMI_ID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
 # Changing the IP Address and DNS Name as per the component
-sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json > record.json
+sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}-${ENV}/" route53.json > record.json
 aws route53 change-resource-record-sets --hosted-zone-id Z037286228DFYMBZCZ58K --change-batch file://record.json | jq 
 
 }
